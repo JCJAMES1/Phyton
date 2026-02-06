7aadd95a303b5f4448f089c67a7e7e74c9ca6ea8
JCJAMES1 <herbiejean123@gmail.com>
2026-02-06 10:27:35 +0800

    Add files via upload

---
+++ week2activity.py
@@ -0,0 +1,16 @@
+fullname = "Jc James"
+age = 18
+height = 1.70
+currently_working = False
+
+print("Fullname:", fullname)
+print(type(fullname))
+
+print("Age:", age)
+print(type(age))
+
+print("Height in meters:", height)
+print(type(height))
+
+print("Currently working?", currently_working)
+print(type(currently_working))
